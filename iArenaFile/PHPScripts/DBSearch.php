e649fdcdb869f58e3480c1f9435a501ea10d5703
jctrubey <jctrubey@outlook.com>
2018-11-01 21:56:01 -0400

    Update iArenaFile/PHPScripts/DBSearch.php

---
+++ iArenaFile/PHPScripts/DBSearch.php
@@ -0,0 +1,33 @@
+<?php
+
+$username = "c";
+$check;
+
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "iarena3300";
+$dbname = "iarenadatabase";
+
+$conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+
+if (mysqli_connect_error()) {
+    die('Connect Error (' . mysqli_connect_errno()) . ')' . mysqli_connect_error();
+} else {
+
+    $result = mysqli_query($conn, "SELECT * FROM iarenadbtable
+    WHERE Username LIKE '%{$username}%'");
+
+    if ($result->num_rows) {
+        $check = true;
+        echo json_encode($check);
+    } else {
+        $check = false;
+        echo json_encode($check);
+    }
+
+    $conn->close();
+}
+
+// header('Location: index.html');
+exit;
+?>
