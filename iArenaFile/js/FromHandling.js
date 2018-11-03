6a4eea1a2330f3d6f4c73f95e03832165f1fc146
jctrubey <jctrubey@outlook.com>
2018-11-03 06:59:42 -0400

    Update iArenaFile/js/FromHandling.js

--- iArenaFile/js/FromHandling.js
+++ iArenaFile/js/FromHandling.js
@@ -1,5 +1,6 @@
 var signup = $("[name = signupform]");
 var usernamelookup;
+var signintest;
 
 function usernamesearch() {
     $('#usrnameerr').html("");
@@ -17,8 +18,7 @@ function usernamesearch() {
                 console.log("database contains this usertname");
                 usernamelookup = true;
                 $('#usrnameerr').html("username already in use");
-            }
-            else if (temp === "false") {
+            } else if (temp === "false") {
                 console.log("database does not contain this username");
                 usernamelookup = false;
             }
@@ -30,14 +30,51 @@ function usernamesearch() {
     });
 }
 
-function validateform() {
-    $('#pwerr').html("");
-        if ($('#password').val() === $('#confirmpassword').val() && usernamelookup === false) {
-            $('#pwerr').html("passwords match");
-            signup.attr('action', 'PHPScripts/SignUpScript.php');
-            return true;
-        } else {
-            $('#pwerr').html("passwords do not match");
-            return false;
+$("#signinform").submit(function (e) {
+    e.preventDefault();
+});
+
+function loginsearch() {
+    console.log("sign in attempt");
+    var validateusrname = $('#unamer').val();
+    var validatepw = $('#pswr').val();
+    console.log(validatepw);
+    console.log(validateusrname);
+    $.ajax({
+        type: "POST",
+        url: "PHPScripts/SignInScript.php",
+        data: {validateusrname, validatepw},
+        success: function (result) {
+            console.log(result);
+            temp = result;
+            console.log(temp);
+            if (temp === "true") {
+                console.log("Sign in success");
+                signintest = true;
+            } else if (temp === "false") {
+                console.log("Sign in fail");
+                signintest = false;
+                $('#signinerr').html("username or password is incorrect");
+            }
+
+        },
+        error: function () {
+            alert("error");
         }
+    });
+}
+
+/**
+ * @return {boolean}
+ */
+function ValidateSignUpForm() {
+    $('#pwerr').html("");
+    if ($('#password').val() === $('#confirmpassword').val() && usernamelookup === false) {
+        $('#pwerr').html("passwords match");
+        signup.attr('action', 'PHPScripts/SignUpScript.php');
+        return true;
+    } else {
+        $('#pwerr').html("passwords do not match");
+        return false;
     }
+}
