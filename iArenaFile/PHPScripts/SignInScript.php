58a38f311c082d0d0a0637b3bf6d0208f5d319e3
jctrubey <jctrubey@outlook.com>
2018-11-03 07:00:06 -0400

    Update iArenaFile/PHPScripts/SignInScript.php

--- iArenaFile/PHPScripts/SignInScript.php
+++ iArenaFile/PHPScripts/SignInScript.php
@@ -1,36 +1,33 @@
-/**
- * Created by WebStorm.
- * User: jctru
- * Date: 11/1/2018
- * Time: 9:33 AM
- */
 <?php
 
-$username = filter_input(INPUT_POST, 'username');
-$password = filter_input(INPUT_POST, 'password');
+$username = $_POST['validateusrname'];
+$password = $_POST['validatepw'];
+$check;
 
 $host = "localhost";
 $dbusername = "root";
 $dbpassword = "iarena3300";
-$dbname = "youtube";
+$dbname = "iarenadatabase";
 
 $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
 
 if (mysqli_connect_error()) {
     die('Connect Error (' . mysqli_connect_errno()) . ')' . mysqli_connect_error();
-}
-else {
-    $sql = "INSERT INTO account (username, password)
-    values ('$username', '$password')";
-    if ($conn->query($sql)) {
-        echo "new record inserted";
+} else {
+
+    $result = mysqli_query($conn, "SELECT * FROM iarenadbtable
+    WHERE Username = '$username' AND Password = '$password'");
+
+    if (mysqli_num_rows($result) != 0) {
+        $check = true;
+        echo json_encode($check);
+    } else {
+        $check = false;
+        echo json_encode($check);
     }
-    else {
-        echo "Eror".$sql."<br>".$conn->error;
-    }
+
     $conn->close();
 }
 
-// header('Location: index.html');
 exit;
 ?>
