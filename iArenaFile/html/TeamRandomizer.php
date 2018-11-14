028b80562e3250074b76155d4074655bf6a0beec
jctrubey <jctrubey@outlook.com>
2018-11-14 03:43:03 -0500

    Update iArenaFile/html/TeamRandomizer.php

--- iArenaFile/html/TeamRandomizer.php
+++ iArenaFile/html/TeamRandomizer.php
@@ -18,27 +18,31 @@
 
 		$teamCounter=0;
 		$divisionCount = 0;
-			echo "<table>";
+			echo "<table class='table'>";
 			while ($rows = mysqli_fetch_array($sql_object)) {
                 $color1 = $rows['color1'];
                 $color2 = $rows['color2'];
 				if($teamCounter%4 == 0 && $divisionCount != 0){
+					echo "</tbody>";
 					echo "</table>";
 					$divisionCount = $divisionCount + 1;
-					echo "<h2>Division ".$divisionCount."</h2>";
-					echo "<table>";
-					echo "<tr><td><div class=$color1></div><div class=$color2></div><div id=teambox3></div>" . ($rows['teamname']) . "</td></tr>";
+                    echo "<table class='table'>";
+                    echo "<thead>";
+					echo "<tr><th>Division ".$divisionCount."</th><th>Wins</th><th>Losses</th><th>Draws</th><th>Total Points</th></tr>";
+					echo "</thead><tbody>";
+					echo "<tr><td><div class=$color1></div><div class=$color2></div><div id=teambox3></div>" . ($rows['teamname']) . "</td><td>".($rows['wins'])."</td><td>".($rows['losses'])."</td><td>".($rows['draws'])."</td><td>".($rows['TotalPoints'])."</td></tr>";
 					$teamCounter = $teamCounter + 1;
-					
 				} elseif ($teamCounter%4 == 0 && $divisionCount == 0){
 					$divisionCount = $divisionCount + 1;
-					echo "<h2>Division ".$divisionCount."</h2>";
-					echo "<table>";
-					echo "<tr><td><div class=$color1></div><div class=$color2></div><div id=teambox3></div>" . ($rows['teamname']) . "</td></tr>";
+                    echo "<table class='table'>";
+                    echo "<thead>";
+                    echo "<tr><th>Division ".$divisionCount."</th><th>Wins</th><th>Losses</th><th>Draws</th><th>Total Points</th></tr>";
+                    echo "</thead><tbody>";
+					echo "<tr><td><div class=$color1></div><div class=$color2></div><div id=teambox3></div>" . ($rows['teamname']) . "</td><td>".($rows['wins'])."</td><td>".($rows['losses'])."</td><td>".($rows['draws'])."</td><td>".($rows['TotalPoints'])."</td></tr>";
 					$teamCounter = $teamCounter + 1;}
 				
 				else {
-					echo "<tr><td><div class=$color1></div><div class=$color2></div><div id=teambox3></div>" . ($rows['teamname']) . "</td></tr>";
+					echo "<tr><td><div class=$color1></div><div class=$color2></div><div id=teambox3></div>" . ($rows['teamname']) . "</td><td>".($rows['wins'])."</td><td>".($rows['losses'])."</td><td>".($rows['draws'])."</td><td>".($rows['TotalPoints'])."</td></tr>";
 					$teamCounter = $teamCounter + 1;
 				}
 			}
