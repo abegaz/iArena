970bed600bb702c4e156e511cdfd5dac60ea4b82
smtrua8946 <smtrua8946@ung.edu>
2018-11-27 04:26:13 -0500

    Update SignInScript.php

--- iArenaFile/PHPScripts/SignInScript.php
+++ iArenaFile/PHPScripts/SignInScript.php
@@ -1,7 +1,7 @@
 <?php
 
 $username = $_POST['validateusrname'];
-$password = $_POST['validatepw'];
+$rawpassword = $_POST['validatepw'];
 $check;
 
 $host = "localhost";
@@ -14,9 +14,11 @@ $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
 if (mysqli_connect_error()) {
     die('Connect Error (' . mysqli_connect_errno()) . ')' . mysqli_connect_error();
 } else {
+	
+	$passwordhash = hash('sha256', $rawpassword);
 
     $result = mysqli_query($conn, "SELECT * FROM iarenadbtable
-    WHERE Username = '$username' AND Password = '$password'");
+    WHERE Username = '$username' AND Password = '$passwordhash'");
 
     if (mysqli_num_rows($result) != 0) {
         $check = true;
