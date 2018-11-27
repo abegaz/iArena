81001e982c01aa8350c9598947e2e1ab532e94b0
jctrubey <jctrubey@outlook.com>
2018-11-27 11:01:46 -0500

    Schedule javascript

--- iArenaFile/PHPScripts/SchedulePull.php
+++ iArenaFile/PHPScripts/SchedulePull.php
@@ -6,3 +6,13 @@ $dbpassword = "";
 $dbname = "iarenadatabase";
 
 $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+
+$sql_object = mysqli_query($conn, "SELECT * FROM bracketschedule WHERE Winner IS NULL LIMIT 1");
+
+while ($row = mysqli_fetch_object($sql_object)) {
+    echo $row->Team1;
+    echo " vs. ";
+    echo $row->Team2;
+}
+
+mysqli_query($conn, "UPDATE bracketschedule SET Winner = 1 WHERE Winner IS NULL LIMIT 1");
---
+++ iArenaFile/js/ScheduleCreation.js
@@ -0,0 +1,24 @@
+var listitem;
+window.onload = function () {
+    var list = document.getElementsByClassName('matchteams');
+    for (var i = 0; i < list.length; i++) {
+        load();
+        list[i].innerText=listitem;
+    }
+};
+
+function load() {
+    $.ajax({
+        type: "POST",
+        url: "../PHPScripts/SchedulePull.php",
+        async: false,
+        success: function (result) {
+            alert(result);
+            listitem = result;
+            $("#test").html(listitem);
+        },
+        error: function () {
+            alert("error");
+        }
+    });
+}
